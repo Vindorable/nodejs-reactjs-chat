bdfd8735991e9944223cc5b0c5bb723d812f9ef7
Vindorable <j.raj93.designer@gmail.com>
2024-01-06 10:34:15 +0800

    Test run backend server. Also install visual studio code extension 'REST Client' by Huachao Mao.

---
+++ nodejs-reactjs-chat/backend-nodejs/index.js
@@ -0,0 +1,15 @@
+// Boiler-plate code from https://blog.chatengine.io/fullstack-chat/nodejs-reactjs
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  return res.json({ username: username, secret: "sha256..." });
+});
+
+app.listen(3001);
