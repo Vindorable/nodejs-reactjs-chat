e252e3ff2928168a1461f06aa4e7cd295ae6bdd0
Vindorable <j.raj93.designer@gmail.com>
2024-01-07 11:02:49 +0800

    Setup chats page ui with 2 different styles.  "npm i react-chat-engine-advanced"  "npm install react-chat-engine-pretty --save"

--- nodejs-reactjs-chat/frontend-reactjs/src/ChatsPage.jsx
+++ nodejs-reactjs-chat/frontend-reactjs/src/ChatsPage.jsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from "react-chat-engine-advanced";
+import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 
 // ---------------------------------------------------------
 
-const ChatsPage = () => {
+const ChatsPage = (props) => {
+  const chatProps = useMultiChatLogic(
+    "ada59c5a-b522-49a7-bc06-47356b160e92",
+    props.user.username,
+    props.user.secret
+  )
+
+  const [pretty, setPretty] = useState(false);
+
   return (
     <>
-      ChatsPage...
+      <div style={{ height: "100dvh" }}>
+        {pretty ?
+          <>
+            <PrettyChatWindow
+              projectId="ada59c5a-b522-49a7-bc06-47356b160e92"
+              username={props.user.username}
+              secret={props.user.secret}
+              style={{ height: "100%" }}
+            />
+          </>
+          :
+          <>
+            <MultiChatSocket {...chatProps} />
+            <MultiChatWindow {...chatProps} style={{ height: "100%" }} />
+          </>
+        }
+      </div>
     </>
   );
 }
